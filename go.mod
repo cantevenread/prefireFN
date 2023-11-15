b7e4f750f3befcc662f59af01488dcb3d401c7cf
CanRead <78627491+cantevenread@users.noreply.github.com>
2023-11-14 20:01:32 -0500

    h

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/cantevenread/prefireFN
-
-go 1.21.4
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use enigo::*;
+fn main() {
+    let mut enigo = enigo::Enigo::new();
+    enigo.mouse_move_to(0, 955);
+    println!("Moved")
+}
